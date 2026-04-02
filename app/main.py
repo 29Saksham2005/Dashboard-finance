895c839d7a1f59c4d67f8cda828e5961d3892bea
Saksham <sakshamsingh392005@gmail.com>
2026-04-03 00:59:31 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-
+from app.api.routes import health
 app = FastAPI(
     title="Finanace Dashboard Backend",
     description="Backend Api for finance data and role based access control.",
     version="1.0.0"
 )
+app.include_router(health.router)
 
 @app.get("/")
 def root():
