62fdcb903141eb05d3889c0994bcede998aaa7a6
Saksham <sakshamsingh392005@gmail.com>
2026-04-03 20:19:04 +0530

    final_cleanup

--- app/api/routes/schema_test.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.schemas.record import RecordCreate, RecordResponse
-
-router =APIRouter(prefix="/schema-test",tags=["Schema Test"])
-
-@router.post("/record",response_model=RecordCreate)
-def test_record_schema(payload: RecordCreate):
-    return payload
--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-from app.api.routes import health, test_db, schema_test, auth, users, records, dashboard
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from app.api.routes import health, auth, users, records, dashboard
 from app.core.database import Base, engine
+from app.core.exceptions import (
+    http_exception_handler,
+    validation_exception_handler,
+    generic_exception_handler
+)
+
 
 #Import models so SQLAlchemy registers them
-from app.models import user, FinancialRecord
+from app.models import User, FinancialRecord
 
 Base.metadata.create_all(bind=engine)
 
@@ -13,9 +22,10 @@ app = FastAPI(
     description="Backend Api for finance data and role based access control.",
     version="1.0.0"
 )
+app.add_exception_handler(StarletteHTTPException, http_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(Exception, generic_exception_handler)
 app.include_router(health.router)
-app.include_router(test_db.router)
-app.include_router(schema_test.router)
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(records.router)
